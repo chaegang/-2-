ac1a8cd62c42f694301aab369fd4162281bc07e5
smoothle <165007099+smoothle@users.noreply.github.com>
2024-11-28 18:28:11 +0000

    2nd_file

--- module_a.py
+++ module_a.py
@@ -1,11 +1,10 @@
 import csv 
 import pandas as pd
 
-#return [표준점수, 남자, 여자]
 
 def extract_data(file_path, subject):
-    #dataList - [score, 남자, 여자] 
-
+    
+    #return [표준점수, 남자, 여자]
 
     try:
         df = pd.read_csv(file_path, encoding='utf-8')
