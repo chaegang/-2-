26a3e2a07342b10cde87a0a1c365744e0e6addf3
smoothle <165007099+smoothle@users.noreply.github.com>
2024-11-30 10:56:18 +0000

    수정본

--- module_a.py
+++ module_a.py
@@ -5,13 +5,14 @@ import pandas as pd
 def extract_data(file_path, subject):
     
     #return [표준점수, 남자, 여자]
+    #subject_list : 과목 목록
 
     try:
         df = pd.read_csv(file_path, encoding='utf-8')
     except UnicodeDecodeError:
         df = pd.read_csv(file_path, encoding='cp949')
 
-    
+    subject_list = df['유형'].drop_duplicates().tolist()
     subject_data = df[df['유형'] == subject]
     subject_sorted = subject_data.sort_values(by='표준점수', ascending=False)
     data = subject_sorted[['표준점수','남자', '여자']]
