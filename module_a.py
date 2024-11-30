a709e1b6cd8275b4d2e2869046d9b4bb803a18fd
smoothle <165007099+smoothle@users.noreply.github.com>
2024-11-30 12:06:38 +0000

    update_a.py

--- module_a.py
+++ module_a.py
@@ -2,17 +2,28 @@ import csv
 import pandas as pd
 
 
-def extract_data(file_path, subject):
-    
-    #return [표준점수, 남자, 여자]
+def extract_subjecT(file_path):
+
     #subject_list : 과목 목록
 
     try:
         df = pd.read_csv(file_path, encoding='utf-8')
     except UnicodeDecodeError:
         df = pd.read_csv(file_path, encoding='cp949')
-
+    
     subject_list = df['유형'].drop_duplicates().tolist()
+    return subject_list
+
+
+def extract_data(file_path, subject):
+    
+    #return [표준점수, 남자, 여자]
+
+    try:
+        df = pd.read_csv(file_path, encoding='utf-8')
+    except UnicodeDecodeError:
+        df = pd.read_csv(file_path, encoding='cp949')
+
     subject_data = df[df['유형'] == subject]
     subject_sorted = subject_data.sort_values(by='표준점수', ascending=False)
     data = subject_sorted[['표준점수','남자', '여자']]
