526a15b273efee52116974fe8c6bdb5bad9ec372
chaegang <110167462+chaegang@users.noreply.github.com>
2024-11-30 12:46:49 +0000

    메인

--- main.py
+++ main.py
@@ -6,3 +6,28 @@ import module_b
 #과목 리스트 만들고 사용자 과목 입력 받기
 #과목에 따른 표준점수, 남자, 여자 리스트 만들기
 #module_b.score_distribution_graph -> 그래프 그리기
+
+
+subject_list = module_a.extract_subjecT('https://github.com/chaegang/computer_programming_II/raw/refs/heads/main/20231231.csv')
+
+print("과목:"," ".join(subject_list))
+user_subject = input("과목 입력")
+        
+while True:
+    if user_subject not in subject_list:
+        user_subject = input("다시 과목 입력")
+    else:
+        break
+
+data_list = module_a.extract_data('https://github.com/chaegang/computer_programming_II/raw/refs/heads/main/20231231.csv',user_subject)
+
+scores = []
+males_list = []
+females_list = []
+
+for score, male, female in data_list:
+    scores.append(score)
+    males_list.append(male)
+    females_list.append(female)
+
+module_b.score_distribution_graph(user_subject, scores, males_list, females_list, font = 'NanumGothic')
